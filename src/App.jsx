a4d0eaf25cf7b9f2648956f825b8f089b724e3c7
Yurdoy <yurdoy@gmail.com>
2025-06-21 20:54:06 +0200

    Routing created

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
+import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Article from "./components/Article";
+import ArticleList from "./components/ArticleList";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <Link to="/">Main</Link>
+        <Link to="/articles"></Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/articles" element={<ArticleList />} />
+        <Route path="/article/:id" element={<Article />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
